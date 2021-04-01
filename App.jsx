3f273d59e12f0fd2c1ef120adf8791c636aa5380
Jyotiraj Dixit <jyotirajdixit50@gmail.com>
2021-04-01 10:56:26 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+//import Car from './Car';
+//import CarDetails from './CarDetails';
+//import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+//import './App.css';
+//import CompareCars from './CompareCars';
+
+function App() {
+  return (
+    <h1>app</h1>
+  );
+}
+
+export default App;
+/*
+<Router>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact component={Car} />
+          <Route path="/car" exact component={Car} />
+          <Route path="/car/:id" component={CarDetails} />
+          <Route path="/comparecars" component={CompareCars} />
+        </Switch>
+      </div>
+    </Router>
+    */
